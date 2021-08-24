c2e08f736d65ab654a2c4d9944275013efa020f8
mikseoo <mkdevelop5002@gmail.com>
2021-08-25 02:15:33 +0900

    Add menu slider with carousel_slider

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:kisik_app/screen/weekMenu.dart';
 import 'package:date_format/date_format.dart';
@@ -11,6 +12,43 @@ Widget realTime() {
           [yyyy, '년 ', mm, '월 ', dd, '일 ', hh, ':', nn, ':', ss, ' ', am]));
     },
   );
+}
+
+Widget horizontalDragAnimate() {
+  List payloadList = ['아침 메뉴', '점심 메뉴', '저녁 메뉴'];
+  return CarouselSlider(
+      options: CarouselOptions(
+        viewportFraction: 0.7,
+        aspectRatio: 2.0,
+        autoPlay: true,
+        enlargeCenterPage: true,
+      ),
+      items: payloadList.map((payload) {
+        return Builder(builder: (BuildContext context) {
+          return Column(
+            children: <Widget>[
+              Text(
+                payload,
+                style: TextStyle(fontSize: 25),
+              ),
+              Flexible(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.orange,
+                    ),
+                    color: Colors.transparent,
+                  ),
+                  //fit: BoxFit.fill,
+                ),
+              ),
+            ],
+          );
+        });
+      }).toList());
 }
 
 class MyHomePage extends StatefulWidget {
@@ -37,6 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: <Widget>[
             realTime(),
+            horizontalDragAnimate(),
           ],
         ),
       ),
